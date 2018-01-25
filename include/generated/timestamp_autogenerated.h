ca559b2e8ed40232759a66bed1b503a5efbbe5da
shuimu <razhgu@sina.com>
2018-01-25 10:30:56 +0800

    solve the git branch problem, based the nand start

--- include/generated/timestamp_autogenerated.h
+++ include/generated/timestamp_autogenerated.h
@@ -1,4 +1,4 @@
-#define U_BOOT_DATE "Jan 24 2018"
-#define U_BOOT_TIME "21:32:32"
+#define U_BOOT_DATE "Jan 25 2018"
+#define U_BOOT_TIME "10:28:43"
 #define U_BOOT_TZ "+0800"
-#define U_BOOT_DMI_DATE "01/24/2018"
+#define U_BOOT_DMI_DATE "01/25/2018"
