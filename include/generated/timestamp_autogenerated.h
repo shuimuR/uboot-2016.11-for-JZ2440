745bdf2fd5847413b5e42a78481318cf4e629d72
shuimu <razhgu@sina.com>
2018-01-25 09:00:21 +0800

    code can run on NAND, but there's something to do with new environment

--- include/generated/timestamp_autogenerated.h
+++ include/generated/timestamp_autogenerated.h
@@ -1,4 +1,4 @@
-#define U_BOOT_DATE "Jan 24 2018"
-#define U_BOOT_TIME "21:32:32"
+#define U_BOOT_DATE "Jan 25 2018"
+#define U_BOOT_TIME "08:40:19"
 #define U_BOOT_TZ "+0800"
-#define U_BOOT_DMI_DATE "01/24/2018"
+#define U_BOOT_DMI_DATE "01/25/2018"
